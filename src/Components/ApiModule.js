6acd50b6e1a59b22fa4a234c1286d5e8d71dca5a
BSCE22001 <bsce22001@itu.edu.pk>
2024-09-01 18:29:15 +0500

    again

--- src/Components/ApiModule.js
+++ src/Components/ApiModule.js
@@ -13,6 +13,13 @@ export default function ApiModule() {
     <>
       {state?.weather?.isLoading ? (
         <Pendulum />
+      ) : state?.weather?.isError ? (
+        <>
+          <div className="justify-center items-center text-center text-white font-dosis font-semibold text-3xl">
+            <p>Some Error Occurred While fetching the data</p>
+            <p>Please try again after some time</p>
+          </div>
+        </>
       ) : state?.weather?.data?.error ? (
         <>
           <div className="mt-5 mb-5">
