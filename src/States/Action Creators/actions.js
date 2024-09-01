f3f7974bb6070ca18f90644f2731542dc5e8b041
Muhammad-Miqdad-Ahmad <muhammadmiqdadahmad@gmail.com>
2024-09-01 19:08:12 +0500

    https

--- src/States/Action Creators/actions.js
+++ src/States/Action Creators/actions.js
@@ -18,7 +18,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const getWeather = createAsyncThunk('getWeather', async (prams) => {
-    const result = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${prams[0]}&q=${prams[1]}&days=7&aqi=no&alerts=no`)
+    const result = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${prams[0]}&q=${prams[1]}&days=7&aqi=no&alerts=no`)
         .then()
         .catch((error) => {
             console.error("ERROR fetching API data: ", error);
